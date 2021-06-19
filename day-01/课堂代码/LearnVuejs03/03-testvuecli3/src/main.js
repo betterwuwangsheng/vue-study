95fa324af5f4524e18b3882508ea3df1c3a5d3fc
betterwuwangsheng <349807102@qq.com>
2021-06-19 08:57:07 +0800

    vue.config.js自己设置配置

--- day-01/课堂代码/LearnVuejs03/03-testvuecli3/src/main.js
+++ day-01/课堂代码/LearnVuejs03/03-testvuecli3/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//是否显示提示信息
 Vue.config.productionTip = false
 
+
+// new Vue({
+//   el: '#app',
+//   render: function (h) {
+//     return h(App)
+//   }
+// })
 new Vue({
   render: h => h(App),
 }).$mount('#app')
